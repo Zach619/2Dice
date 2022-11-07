28623e278497c0458c8eac73c27beeaea2e3a211
Zach619 <112730663+Zach619@users.noreply.github.com>
2022-11-07 14:23:28 -0800

    Update DieN.java

--- DieN/DieN.java
+++ DieN/DieN.java
@@ -1,15 +1,19 @@
 public class DieN {
     private int value;
     private int sides;
-    public DieN() {
+    public DieN(int sides) {
         this.roll();
+        this.sides = sides;
     }
+
     public void roll() {
-        this.value = (int)(Math.random()*this.sides + 1);
+        this.value = (int)(Math.random()*sides) + 1;
     }
+
     public int getValue() {
         return value;
     }
+
     public int rollandgetValue() {
         roll();
         return getValue();
