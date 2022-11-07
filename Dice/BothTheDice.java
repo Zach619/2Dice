b9662ccb3ba0ed4fc9a1b101789eeba833a0b924
Zach619 <112730663+Zach619@users.noreply.github.com>
2022-11-07 13:55:11 -0800

    Update BothTheDice.java

--- Dice/BothTheDice.java
+++ Dice/BothTheDice.java
@@ -2,20 +2,21 @@ public class BothTheDice {
     private Die6 Die1;
     private Die6 Die2;
     public BothTheDice() {
-        Die1 = new Die6;
-        Die2 = new Die6;
+        Die1 = new Die6();
+        Die2 = new Die6();
         this.roll();
     }
       public int getValue() {
-        return Die1.getValue()+Die2.getvalue();
+        return Die1.getValue()+Die2.getValue();
     }
     public int getValue(int dieNumber) {
         if(dieNumber == 1) {
-            return this.die1.getValue();
+            return this.Die1.getValue();
         }else if(dieNumber == 2) {
-            return this.die2.getValue();
+            return this.Die2.getValue();
         }   
-        }
+    }
+    
     public void roll() {
         this.Die1.roll();
         this.Die2.roll();
